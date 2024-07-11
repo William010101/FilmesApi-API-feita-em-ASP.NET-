3b308d657f268cf28bca73a43b69378df0eefebe
William Guesser <williamlourenco273@gmail.com>
2024-07-11 17:07:06 -0300

    GET listFilmes

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,9 +1,17 @@
-﻿namespace FilmesApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "titulo é obrigatório") ]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "titulo é obrigatório")]
+        [MaxLength(50, ErrorMessage="O tamanho do gênero não pode exceder 50")]
         public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "a duração não pode ser maior que 600 min")]
         public int Duracao { get; set; }
     }
 }
